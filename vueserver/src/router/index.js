e277ead552c41e0257e2698ecbd02758c0f431c5
jiin0104 <jiin21c@naver.com>
2023-07-27 17:36:05 +0900

    필터링페이지 수정중

--- vueserver/src/router/index.js
+++ vueserver/src/router/index.js
@@ -1,23 +1,29 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Main from '../views/Main.vue'
-import Login from '../components/Login.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Main from "../views/Main.vue";
+import Filtering from "../views/Filtering.vue";
+import Login from "../components/Login.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'main',
-    component: Main
+    path: "/",
+    name: "main",
+    component: Main,
   },
   {
-    path: '/login',
-    name: 'login',
-    component: Login
-  }
-]
+    path: "/login",
+    name: "login",
+    component: Login,
+  },
+  {
+    path: "/Filtering",
+    name: "Filtering",
+    component: Filtering,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
--- vueserver/src/views/Main.vue
+++ vueserver/src/views/Main.vue
@@ -1,17 +1,23 @@
 <template>
   <div>
     <div class="background">
-      <br>
-      <v-container class="header">
-        wavecinema
-      </v-container><br>
-      <div class="maintext">당신을 위한<br>영화를 찾아드립니다</div><br>
-      <div style="width: 370px; margin:0 auto;"><button class="click">click !</button></div>
-      <br><br><br>
+      <br />
+      <v-container class="header"> wavecinema </v-container><br />
+      <div class="maintext">당신을 위한<br />영화를 찾아드립니다</div>
+      <br />
+      <div style="width: 370px; margin: 0 auto">
+        <button class="click" @click="pageLink">click !</button>
+      </div>
+      <br /><br /><br />
       <div class="infotext">지금 상영중인 추천 영화</div>
       <!--이미지슬라이더-->
       <div class="slider">
-        <v-carousel cycle height="400" hide-delimiter-background show-arrows="hover">
+        <v-carousel
+          cycle
+          height="400"
+          hide-delimiter-background
+          show-arrows="hover"
+        >
           <v-carousel-item v-for="(slide, i) in slides" :key="i">
             <v-sheet img :src="img[i]" height="100%">
               <div class="d-flex fill-height justify-center align-center">
@@ -28,32 +34,25 @@
 </template>
 <script>
 export default {
-  name: '',
+  name: "",
   components: {},
   data() {
     return {
-      img: [
-        '../assets/al.jpg',
-
-      ],
-      slides: [
-        '스타이즈본',
-        '알라딘',
-        '라라랜드',
-        '바빌론',
-        '바비',
-      ],
-
+      img: ["../assets/al.jpg"],
+      slides: ["스타이즈본", "알라딘", "라라랜드", "바빌론", "바비"],
     };
   },
-
-}
+  methods: {
+    pageLink() {
+      this.$router.push({ path: "Filtering" });
+    },
+  },
+};
 </script>
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Black+Han+Sans&display=swap');
-@import url('https://fonts.googleapis.com/css2?family=Black+Han+Sans&family=Dokdo&display=swap');
-@import url('https://fonts.googleapis.com/css2?family=Black+Han+Sans&family=Dokdo&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Black+Han+Sans&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Black+Han+Sans&family=Dokdo&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Black+Han+Sans&family=Dokdo&display=swap");
 
 @import "../css/main.css";
 </style>
-
