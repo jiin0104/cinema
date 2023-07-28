a4573a48fc25a28e35d7395e49358e98828d5156
jiin0104 <jiin21c@naver.com>
2023-07-28 17:08:14 +0900

    index.js

--- vueserver/src/router/index.js
+++ vueserver/src/router/index.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from "vue-router";
 import Main from "../views/Main.vue";
 import Filtering from "../views/Filtering.vue";
 import Filtering2 from "../views/Filtering2.vue";
+import Filtering3 from "../views/Filtering3.vue";
+import Filtering4 from "../views/Filtering4.vue";
 import Login from "../components/Login.vue";
 
 const routes = [
@@ -24,6 +26,16 @@ const routes = [
     path: "/Filtering2",
     name: "Filtering2",
     component: Filtering2,
+  },
+  {
+    path: "/Filtering3",
+    name: "Filtering3",
+    component: Filtering3,
+  },
+  {
+    path: "/Filtering4",
+    name: "Filtering4",
+    component: Filtering4,
   },
 ];
 
