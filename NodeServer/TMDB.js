581204a11e1b776ede37c3771fd0800402d43cb1
hongpac <cjsdn6466@naver.com>
2023-08-01 16:22:19 +0900

    no message

--- NodeServer/TMDB.js
+++ NodeServer/TMDB.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 
 const options = {
   method: "GET",
-  url: "https://api.themoviedb.org/3/genre/movie/list?api_key=49ba50092811928efb84febb9d68823f&language=ko",
+  url: "https://api.themoviedb.org/3/discover/movie?with_genres=장르번호(후에 여기에 vue에서 장르필터링 버튼을 누르면 버튼에 저장된 장르 번호가 여기에 들어가게 해야됨)&api_key=49ba50092811928efb84febb9d68823f",
   headers: {
     accept: "application/json",
     Authorization:
@@ -18,3 +18,26 @@ axios
   .catch(function (error) {
     console.error(error);
   });
+
+//장르 번호들
+  {"genres":[
+  {"id":28,"name":"Action"},
+  {"id":12,"name":"Adventure"},
+  {"id":16,"name":"Animation"},
+  {"id":35,"name":"Comedy"},
+  {"id":80,"name":"Crime"},
+  {"id":99,"name":"Documentary"},
+  {"id":18,"name":"Drama"},
+  {"id":10751,"name":"Family"},
+  {"id":14,"name":"Fantasy"},
+  {"id":36,"name":"History"},
+  {"id":27,"name":"Horror"},
+  {"id":10402,"name":"Music"},
+  {"id":9648,"name":"Mystery"},
+  {"id":10749,"name":"Romance"},
+  {"id":878,"name":"Science Fiction"},
+  {"id":10770,"name":"TV Movie"},
+  {"id":53,"name":"Thriller"},
+  {"id":10752,"name":"War"},
+  {"id":37,"name":"Western"}
+]}
