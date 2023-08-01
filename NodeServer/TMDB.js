34931891e1cccffe9542b615f1e89c109dd66948
hongpac <cjsdn6466@naver.com>
2023-08-01 17:39:42 +0900

    no message

--- NodeServer/TMDB.js
+++ NodeServer/TMDB.js
@@ -2,6 +2,9 @@
 // const axios = require("axios");
 // const options = {
 //   method: "GET",
+
+//예시 url https://api.themoviedb.org/3/discover/movie?with_genres=28&api_key=49ba50092811928efb84febb9d68823f
+
 //   url: "https://api.themoviedb.org/3/discover/movie?with_genres=장르번호(후에 여기에 vue에서 장르필터링 버튼을 누르면 버튼에 저장된 장르 번호가 여기에 들어가게 해야됨)&api_key=49ba50092811928efb84febb9d68823f",
 //   headers: {
 //     accept: "application/json",
@@ -19,33 +22,36 @@
 //     console.error(error);
 //   });
 
-//장르 번호들
-//   {"genres":[
-//   {"id":28,"name":"Action"},
-//   {"id":12,"name":"Adventure"},
-//   {"id":16,"name":"Animation"},
-//   {"id":35,"name":"Comedy"},
-//   {"id":80,"name":"Crime"},
-//   {"id":99,"name":"Documentary"},
-//   {"id":18,"name":"Drama"},
-//   {"id":10751,"name":"Family"},
-//   {"id":14,"name":"Fantasy"},
-//   {"id":36,"name":"History"},
-//   {"id":27,"name":"Horror"},
-//   {"id":10402,"name":"Music"},
-//   {"id":9648,"name":"Mystery"},
-//   {"id":10749,"name":"Romance"},
-//   {"id":878,"name":"Science Fiction"},
-//   {"id":10770,"name":"TV Movie"},
-//   {"id":53,"name":"Thriller"},
-//   {"id":10752,"name":"War"},
-//   {"id":37,"name":"Western"}
-// ]}
+장르 번호들
+  {"genres":[
+  {"id":28,"name":"Action"},
+  {"id":12,"name":"Adventure"},
+  {"id":16,"name":"Animation"},
+  {"id":35,"name":"Comedy"},
+  {"id":80,"name":"Crime"},
+  {"id":99,"name":"Documentary"},
+  {"id":18,"name":"Drama"},
+  {"id":10751,"name":"Family"},
+  {"id":14,"name":"Fantasy"},
+  {"id":36,"name":"History"},
+  {"id":27,"name":"Horror"},
+  {"id":10402,"name":"Music"},
+  {"id":9648,"name":"Mystery"},
+  {"id":10749,"name":"Romance"},
+  {"id":878,"name":"Science Fiction"},
+  {"id":10770,"name":"TV Movie"},
+  {"id":53,"name":"Thriller"},
+  {"id":10752,"name":"War"},
+  {"id":37,"name":"Western"}
+]}
 
 //뽑아온 영화 디테일 가져오기
 // const axios = require('axios');
 // const options = {
 //   method: 'GET',
+
+//예시 url https://api.themoviedb.org/3/movie/1083862?api_key=49ba50092811928efb84febb9d68823f
+
 //   url: 'https://api.themoviedb.org/3/movie/영화의고유아이디번호(후에추천해준영화의포스터를 누르면 그 영화의 아이디번호를 여기에다가 넣어줘야됨.)?api_key=49ba50092811928efb84febb9d68823f',
 //   headers: {
 //     accept: 'application/json',
