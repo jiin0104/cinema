4d2c7723ca17857cf6244bfabecb525a1a5c0a94
KitaBlack <jhk709401@gmail.com>
2023-08-03 16:10:38 +0900

    로그인 구현

--- vueserver/src/layout/Header.vue
+++ vueserver/src/layout/Header.vue
@@ -2,7 +2,7 @@
 
   <!-- 로그인 상태일 경우 -->
 
-  <v-card v-if="isLogin === true">
+  <v-card v-if="isLogin">
     <v-layout>
       <v-navigation-drawer v-model="drawer" temporary>
         <v-list-item prepend-icon="mdi-menu" title="더보기 메뉴"></v-list-item>
@@ -16,7 +16,7 @@
         </v-list>
       </v-navigation-drawer>
       <v-main style="height: 100px; background-color: black;">
-        <div class="d-flex justify-center align-center h-100" style="margin-right: 400px;">
+        <div class="d-flex justify-center align-center h-100" style="float: right; margin-right: 45%;">
           <div class="d-flex justify-center align-center h-100 header"><img src="logo.png"
               style="width: 200px; height: 50px" />
           </div>
@@ -40,7 +40,6 @@
         <v-list density="compact" nav>
           <v-list-item @click="$router.push('/')" prepend-icon="mdi-home" title="홈" value="/"></v-list-item>
           <v-list-item @click="$router.push('/login')" prepend-icon="mdi-login" title="로그인" value="login"></v-list-item>
-          <v-list-item @click=Logout prepend-icon="mdi-logout" title="로그아웃" value="logout"></v-list-item>
         </v-list>
 
       </v-navigation-drawer>
--- vueserver/src/store/index.js
+++ vueserver/src/store/index.js
@@ -10,6 +10,16 @@ const store = createStore({
     };
   },
   mutations: {
+      //Mutation을 통해서만 State를 변경. 함수작성. Mutations에 접근하는 것은 Component의 Methods 영역내에서 가능. 기본 접근 방법: this.$store.commit('경로명/함수명')
+   // 저장된 유저 정보와 불러온 유저 정보가 일치하는지 확인
+  loginSuccess(state) {
+    state.isLogin = true
+    state.isLoginError = false
+  },
+  logOut(state) {
+    state.isLogin = false
+    state.isLoginError = false
+  },
   },
   modules: {
     loginStore: loginStore
