6ba86e52bd9e67f926ab72e5668b51ccb8ee1fd8
dogonem2 <aquakyesang@naver.com>
2023-08-15 16:51:37 +0900

    오류 수정

--- NodeServer/app.js
+++ NodeServer/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const dbPool = require("./db.js"); //db가 필요한 작업에서 끌어다 쓸 변수 정의.
-
+const fs = require("fs");
+const axios = require("axios");
 //익스프레스 객체
 const app = express();
 
@@ -22,11 +23,24 @@ app.use((req, res, next) => {
 });
 app.use(cors(corsOption)); //CORS 미들웨어
 
+
+
+
 //express 서버로 POST 요청을 할 때 input 태그의 value를 전달하기 위해 사용
 //post 방식으로 클라이언트가 요청하는 본문에 있는 value를 넘겨받고 req.body 객체로 만들어주는 미들웨어.
 //넘겨받은 value들은 DB로 전송
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//cosnt를 줘버리면 sql이 고정값을 가져서 쿼리문 여러 개 못 씀.
+let sql = require("./sql.js");
+
+//우리가 쿼리 수정했을 때 바로바로 내역 볼 수 있게.
+fs.watchFile(__dirname + "/sql.js", (curr, prev) => {
+  console.log("sql 변경시 재시작 없이 반영되도록 함.");
+  delete require.cache[require.resolve("./sql.js")];
+  sql = require("./sql.js");
+});
 
 const authRouter = require("./routes/auth.js");
 const movieRouter = require("./routes/movie");
@@ -36,6 +50,58 @@ app.use("/auth", authRouter);
 app.use("/movie", movieRouter);
 app.use("/mypage", mypageRouter);
 
+//우리가 작성할 엔드포인트 기본틀을 제시. 다른 엔드포인트 작성할 때 /api를 써도 되고 생략해도 됨.
+app.post("/apirole/:alias", async (request, res) => {
+  if (!request.session.email) {
+    return res.status(401).send({
+      error: "You need to login.",
+    });
+  }
+
+  try {
+    res.send(await req.db(request.params.alias));
+  } catch (err) {
+    res.status(500).send({
+      error: err,
+    });
+  }
+});
+
+app.post("/api/:alias", async (request, res) => {
+  try {
+    res.send(
+      await req.db(request.params.alias, request.body.param, request.body.where)
+    );
+  } catch (err) {
+    res.status(500).send({
+      error: err,
+    });
+  }
+});
+
+
+//우리가 쿼리 수정했을 때 바로바로 내역 볼 수 있게.
+fs.watchFile(__dirname + "/sql.js", (curr, prev) => {
+  console.log("sql 변경시 재시작 없이 반영되도록 함.");
+  delete require.cache[require.resolve("./sql.js")];
+  sql = require("./sql.js");
+});
+
+// req 객체 생성, DB 연동
+const req = {
+  async db(alias, param = [], where = "") {
+    return new Promise((resolve, reject) =>
+      dbPool.query(sql[alias].query + where, param, (error, rows) => {
+        if (error) {
+          if (error.code != "ER_DUP_ENTRY") console.log(error);
+          resolve({
+            error,
+          });
+        } else resolve(rows);
+      })
+    );
+  },
+};
 // 서버 실행
 app.listen(3000, () => {
   console.log("port 3000에서 서버구동");
