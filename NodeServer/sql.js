5d49800028e1e762d21123bfb2bf7ba47c2e0a41
jiin0104 <jiin21c@naver.com>
2023-08-16 17:36:07 +0900

    추천목록에 있는 영화정보 불러오기

--- NodeServer/sql.js
+++ NodeServer/sql.js
@@ -2,9 +2,9 @@ module.exports = {
   userin: {
     query: `SELECT * from user where USER_ID=?`,
   },
-  recList: {
-    query: `SELECT MOVIE_TITLE, MOVIE_POSTER, MOVIE_NUM FROM movies`,
-  },
+  // recList: {
+  //   query: `SELECT MOVIE_TITLE, MOVIE_POSTER, MOVIE_NUM FROM movies`,
+  // },
   modList2: {
     query: ` SELECT
     MOVIE_NUM,
@@ -31,11 +31,20 @@ module.exports = {
     query: `select * from movies`,
   },
 
-
   emojipath: {
     query: `SELECT json_extract(emoji, '$.emoji1','$.emoji2', '$.emoji3','$.emoji4') FROM recommend where USER_NUM = (select USER_NUM from user where USER_ID = ?)`,
   },
   emojipath2: {
     query: `SELECT * FROM recommend where USER_NUM = (select USER_NUM from user where USER_ID = ?)`,
+  },
+  recList: {
+    query: `SELECT MOVIE_TITLE, MOVIE_POSTER, MOVIE_NUM
+    FROM movies
+    WHERE MOVIE_NUM IN (
+      SELECT
+        JSON_UNQUOTE(JSON_EXTRACT(MOVIE_NUM, '$[0]')) AS movie_id
+      FROM recommend
+      WHERE RC_NUM = 47
+    );`, //임으로 RC_NUM지정해서 불러오는지 확인해봄.
   },
 };
