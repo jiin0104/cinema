b6d917f90754aba3823fefd2f8fce0758ada965e
hongpac <cjsdn6466@naver.com>
2023-08-17 15:40:48 +0900

    no message

--- NodeServer/routes/movie.js
+++ NodeServer/routes/movie.js
@@ -16,11 +16,11 @@ router.post("/filtervalues", async (request, res) => {
     // 데이터베이스 연결 생성
     const connection = await dbPool.promise();
 
-    //선택한 장르를 이용하기 위해서 장르를 and이용해서 매칭
+    //선택한 장르를 이용하기 위해서 장르를 and,or이용해서 매칭
     const genresQuery = selectGenres
       .map(
         (genre) =>
-          `GENRE1 = '${genre}' and GENRE2 = '${genre}' and GENRE3 = '${genre}' and GENRE4 = '${genre}'`
+          `GENRE1 = '${genre}' OR GENRE2 = '${genre}' OR GENRE3 = '${genre}' OR GENRE4 = '${genre}'`
       )
       .join(" AND ");
 
