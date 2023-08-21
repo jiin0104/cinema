d3aa42440420117b1de70408ba364f04942dd932
KitaBlack <jhk709401@gmail.com>
2023-08-21 16:28:54 +0900

    다수 추천 영화 리스트 UI 수정

--- vueserver/src/views/bigData.vue
+++ vueserver/src/views/bigData.vue
@@ -12,43 +12,61 @@
               overflow: auto;
               width: 1500px;
               height: 550px;
-              padding-left: 340px;
             "
           >
-            <div style="display: inline-block">
-              <div v-for="(rec, ind) in recList" :key="rec" :virtualIndex="ind">
-                <div
-                  style="
-                    position: relative;
-                    top: 20px;
-                    margin: 3px;
-                    height: auto;
-                  "
+          <!--카드-->
+
+          <div
+            v-for="(rec, index) in recList"
+            :key="rec.MOVIE_NUM"
+            style="margin-left: 145px"
+          >
+            <div
+              v-if="index < 20"
+              style="
+                position: relative;
+                left: 45px;
+                margin: 5px;
+                display: inline-block;
+                float: left;
+              "
+            >
+              <v-layout>
+                <v-card
+                  style="max-width: 270px; max-height: 450px; height: 450px; margin-bottom: 30px;"
                 >
-                  <img
-                    :src="`/download/${rec.POSTER[0]}`"
-                    style="height: 100px; width: 100px"
-                    @click="openModal(rec)"
+                  <v-img
+                    :src="`/download/${rec.MOVIE_POSTER}`"
+                    height="300px"
+                    width="270px"
                   />
-                  <img
-                    :src="`/download/${rec.POSTER[1]}`"
-                    style="height: 100px; width: 100px"
-                    @click="openModal(rec)"
-                  />
-                  <img
-                    :src="`/download/${rec.POSTER[2]}`"
-                    style="height: 100px; width: 100px"
-                    @click="openModal(rec)"
-                  />
-                  <img
-                    :src="`/download/${rec.POSTER[3]}`"
-                    style="height: 100px; width: 100px"
-                    @click="openModal(rec)"
-                  />
-                </div>
-              </div>
+
+                  <div class="r_title">
+                    <div>
+                      <div class="headline">{{ rec.MOVIE_TITLE }}</div>
+                    </div>
+                  </div>
+                  <div class="recbtn">
+                    <v-btn
+                      class="infotext"
+                      variant="tonal"
+                      style="
+                        color: white;
+                        background-color: #3742fa;
+                        width: 150px;
+                        height: 30px;
+                        margin: 60px;
+                      "
+                      @click="openModal(rec)"
+                    >
+                      상세보기
+                    </v-btn>
+                  </div>
+                </v-card>
+              </v-layout>
             </div>
           </div>
+        </div>
           <!--카드 끝-->
 
           <!--모달창-->
@@ -63,7 +81,11 @@
                       width="170px"
                     ></v-img>
                   </div>
-                  <div style="font-size: 35px; text-align: center">
+                  <div
+                    style="
+                      font-size: 35px;
+                      text-align: center;
+                    ">
                     {{ selectedMovie.MOVIE_TITLE }}
                   </div>
                 </div>
@@ -71,12 +93,12 @@
                   <p>장르: {{ modList2[0].GENRE1 }},{{ modList2[0].GENRE2 }}</p>
                   <p>개봉일: {{ modList2[0].FORMATTED_RELEASE }}</p>
                   <p>감독: {{ modList2[0].MOVIE_DIRECTOR }}</p>
-                  <p>주연: {{ modList2[0].MOVIE_ACTORS }},</p>
+                  <p>주연: {{ modList2[0].MOVIE_ACTORS }}</p>
                   <p>평점: {{ modList2[0].MOVIE_SCORE }}/100</p>
                 </div>
               </div>
               <div>
-                <div style="margin: 13px">
+                <div style="margin: 10px">
                   <form>
                     한줄리뷰
                     <div class="review">
@@ -85,74 +107,28 @@
                       {{ modList2.USER_ID }} : {{ modList2.REVIEW_COMMENT }}
                     </div>
                   </form>
-                  <form>
-                    <p>
-                      <textarea
-                        class="rectextB"
-                        placeholder="리뷰를 작성해주세요.(30자 이내)"
-                        name=""
-                        id=""
-                        maxlength="30"
-                      ></textarea>
-                      <v-btn
-                        @click="login"
-                        class="infotext"
-                        variant="tonal"
-                        style="
-                          color: white;
-                          background-color: rgb(57, 103, 255);
-                          height: 88px;
-                          width: 105px;
-                          left: 10px;
-                          bottom: 38px;
-                        "
-                      >
-                        등록
-                      </v-btn>
-                    </p>
-                  </form>
                   <v-btn
                     class="infotext"
+                    @click="close_toggle()"
                     variant="tonal"
                     style="
                       color: white;
-                      background-color: rgb(57, 103, 255);
+                      background-color: #3742fa;
                       height: 40px;
                       width: 70px;
                       font-size: 23px;
                       border-radius: 8px;
                       float: right;
                       margin-right: 5px;
+                      margin-top: 15px;
                       margin-bottom: 20px;
-                      margin-top: -15px;
-                    "
-                    @click="close_toggle()"
-                    >닫기
+                     ">닫기
                   </v-btn>
                 </div>
               </div>
             </div>
           </div>
           <!--모달창 끝-->
-          <div class="recB">
-            <v-btn
-              class="infotext"
-              variant="tonal"
-              style="
-                color: white;
-                background-color: #3742fa;
-                height: 50px;
-                width: 210px;
-                margin: 20px;
-                position: relative;
-                top: 300px;
-                right: 400px;
-              "
-              @click="pageLink"
-            >
-              다시 추천받기
-            </v-btn>
-          </div>
         </div>
       </form>
     </div>
@@ -168,30 +144,17 @@ axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
 export default {
   data() {
     return {
-      userinfo: {}, //로그인한 사용자 정보
       logo: "logo.png",
       selectedMovie: null, //선택한 영화 정보 초기화(모달창 눌렀다 닫았을때마다 초기화 되야함)
       recList: [], //추천받은 영화 목록
       modList2: [], //클릭한 영화에 대한 모달창
-      getemoji: [],
     };
   },
   created() {
-    this.Get_user(); //페이지 보여지자마자 사용자 정보 가져오기
-    this.getemo();
     this.Get_RecList(); //추천받은 영화 목록 가져오는 함수 실행
   },
   mounted() {},
   methods: {
-    pageLink() {
-      this.$router.push({ path: "/" }); //다시추천받기 버튼 클릭시 메인으로 이동
-    },
-    async Get_user() {
-      let userinfo = await this.$api("/api/userin", {
-        param: [this.$store.state.userId], //사용자 정보 가져오기
-      });
-      this.userinfo = userinfo[0];
-    },
     async Get_RecList() {
       //추천받은 목록 파라미터값으로 가져오는 함수
       this.recList = await this.$api("/api/UserRList", {
@@ -210,12 +173,11 @@ export default {
       //모달 닫기
       this.selectedMovie = null;
     },
-
-    async getemo() {
-      this.getemoji = await this.$api("/api/emojipath2", {
+    async Get_Movie_List() {
+      //추천 영화 리스트 파라미터값 가져오는 함수
+      this.recList = await this.$api("/api/recList", {
         param: [this.$store.state.userId],
       });
-      console.log(this.getemoji);
     },
   },
 };
